2dfde0c285a5a0bd70c19cbbd6becae6ca0cf1fd
Dylan Malla <dmalla@live.com>
2020-01-11 10:50:47 -0800

    Implemented [SerializeField] for thrust and rotating commands. Created tags to differentiate between 'Friendly', 'Fuel' and 'Death' objects on collision detection.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rcsThrust = 100f;
+
+
+    Rigidbody rigidBody;
+    AudioSource rocketThrustSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rocketThrustSound = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();        Rotate();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                    print("OK");
+                    break;
+            case "Fuel":
+                    print("Fuel");
+                    break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) // Can thrust while rotating
+        {
+            float mainThrustThisFrame = mainThrust * Time.deltaTime;
+
+            rigidBody.AddRelativeForce(Vector3.up * mainThrustThisFrame);
+
+            if (!rocketThrustSound.isPlaying)
+            {
+                rocketThrustSound.Play();
+            }
+        }
+        else
+        {
+            rocketThrustSound.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // Take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; // Resume physics control of rotation
+    }
+}
